bab49fdd729e378c72e8f27855b12c08dca66bc5
Andrew Jordaan <andrew.jordaan@platform45.com>
2022-12-22 17:52:07 +0200

    Move scale logic to class

---
+++ server/ScaleMonitor.js
@@ -0,0 +1,83 @@
+const STATE = {
+  EMPTY_SCALE: 1,
+  EMPTY_BOTTLE: 2,
+  FULL_BOTTLE: 3,
+  PARTIALLY_FULL_BOTTLE: 4
+}
+
+class ScaleMonitor {
+  constructor(weights, maxCapacity, tolerance = 0, scaleOffset = null, currentState = null, debug = false) {
+    this.BOTTLE_WEIGHT = weights
+    this.MAX_BOTTLE_CAPACITY = maxCapacity
+    this.TOLERANCE_GRAMS = tolerance
+    this.scaleOffset = scaleOffset
+    this.currentState = currentState ?? STATE.EMPTY_SCALE
+    this.prevWeight = null
+    this.debug = debug
+  }
+
+  readScale(scaleData) {
+    if (!scaleData) return null;
+
+    if (this.scaleOffset == null) {
+      this.scaleOffset = scaleData
+      this.log('scale offset: ' + this.scaleOffset)
+    }
+
+    const weight = scaleData - this.scaleOffset
+
+    if (!this.scaleHasStabilised()) {
+      this.log('scale not stable')
+      this.prevWeight = weight
+      return null
+    }
+
+    this.log('Weight: ' + weight)
+
+    if (this.approxEqual(weight, 0) && this.currentState != STATE.EMPTY_SCALE) {
+      this.log("BOTTLE REMOVED FROM SCALE")
+      this.currentState = STATE.EMPTY_SCALE
+
+      return { event: 'BOTTLE LIFTED' }
+    }
+    else if (this.approxEqual(weight, this.BOTTLE_WEIGHT.full) && this.currentState == STATE.EMPTY_SCALE) {
+      this.log('FULL BOTTLE ON SCALE', { waterLevel: 100 })
+      this.currentState = STATE.FULL_BOTTLE
+
+      return { event: 'BOTTLE LOWERED', payload: { waterLevel: 100 } }
+    }
+    else if (this.approxEqual(weight, this.BOTTLE_WEIGHT.empty) && this.currentState == STATE.EMPTY_SCALE) {
+      this.currentState = STATE.EMPTY_BOTTLE
+      this.log('empty bottle on scale')
+
+      return { event: 'BOTTLE LOWERED', payload: { waterLevel: 0 } }
+    }
+    else if (weight > (this.BOTTLE_WEIGHT.empty + this.TOLERANCE_GRAMS) && this.currentState == STATE.EMPTY_SCALE) {
+      this.currentState = STATE.PARTIALLY_FULL_BOTTLE
+      this.log('bottle on scale')
+
+      const waterMl = weight - this.BOTTLE_WEIGHT.empty
+      const level = waterMl / this.MAX_BOTTLE_CAPACITY * 100
+      this.log('water level: ' + level)
+
+      return { event: 'BOTTLE LOWERED', payload: { waterLevel: level } }
+    }
+    return null
+  }
+
+  approxEqual(v1, v2) {
+    return Math.abs(v1 - v2) < this.TOLERANCE_GRAMS
+  }
+
+  scaleHasStabilised() {
+    return this.approxEqual(this.prevWeight, weight)
+  }
+
+  log(str) {
+    if(this.debug) {
+      console.log(str)
+    }
+  }
+}
+
+module.exports = ScaleMonitor
--- server/index.js
+++ server/index.js
@@ -3,27 +3,20 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { SerialPort } = require('serialport')
-const serialPortname = '/dev/tty.usbserial-14540'
-const port = new SerialPort({ path: serialPortname, baudRate: 57600 })
-let scaleOffset = null
-
-const STATE = {
-  EMPTY_SCALE: 1,
-  EMPTY_BOTTLE: 2,
-  FULL_BOTTLE: 3,
-  PARTIALLY_FULL_BOTTLE: 4
-}
-
-let currentState = STATE.EMPTY_SCALE
+const ScaleMonitor = require('./ScaleMonitor')
+const serialPortPath = '/dev/tty.usbserial-14540'
+const port = new SerialPort({ path: serialPortPath, baudRate: 57600 })
 
 const TOLERANCE_GRAMS = 20
 const BOTTLE_WEIGHT = { empty: 430, full: 900 }
 const MAX_BOTTLE_CAPACITY = 450
-let prevWeight = 0
+const CLIENT_URL = 'http://localhost:5173'
+
+const scaleMonitor = new ScaleMonitor(BOTTLE_WEIGHT, MAX_BOTTLE_CAPACITY, TOLERANCE_GRAMS)
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: CLIENT_URL,
     methods: ["GET", "POST"]
   }
 });
@@ -34,62 +27,11 @@ io.on('connection', (socket) => {
 
 port.on('readable', function () {
   const scaleData = parseInt(port.read().toString())
-  if(!scaleData) return;
+  const result = scaleMonitor.readScale(scaleData)
 
-  if(scaleOffset == null) {
-    scaleOffset = scaleData
-    console.log('scale offset: ' + scaleOffset)
-  }
-
-  const weight = scaleData - scaleOffset
-  
-  if(! approxEqual(prevWeight, weight)) {
-    console.log('scale not stable')
-    prevWeight = weight
-    return
-  }
-
-  console.log('Weight: ' + weight)
-
-  if(approxEqual(weight, 0)) {
-
-    if(currentState != STATE.EMPTY_SCALE) {
-      console.log("BOTTLE REMOVED FROM SCALE")
-      currentState = STATE.EMPTY_SCALE
-      io.emit('BOTTLE LIFTED')
-    }
-  }
-
-  // if(approxEqual(weight, BOTTLE_WEIGHT.empty)) {
-
-  //   if(currentState == STATE.EMPTY_SCALE) {
-  //     currentState = STATE.EMPTY_BOTTLE
-  //   console.log('EMPTY BOTTLE PLACED ON SCALE')
-
-  //   io.emit('BOTTLE LOWERED', {waterLevel: 0})
-  //   }
-  // }
-
-
-  if(weight > (BOTTLE_WEIGHT.empty + TOLERANCE_GRAMS) && currentState == STATE.EMPTY_SCALE) {
-    currentState = STATE.EMPTY_BOTTLE
-    console.log('bottle on scale')
-
-    const waterMl = weight - BOTTLE_WEIGHT.empty
-    const level = waterMl / MAX_BOTTLE_CAPACITY * 100
-    console.log('water level: ' + level)
-
-    io.emit('BOTTLE LOWERED', { waterLevel: level })
-  }
-  else if(approxEqual(weight, BOTTLE_WEIGHT.empty) && currentState == STATE.EMPTY_SCALE) {
-    currentState = STATE.EMPTY_BOTTLE
-    console.log('empty bottle on scale')
-    io.emit('BOTTLE LOWERED', { waterLevel: 0 })
-
-  }
-
-  if(approxEqual(weight, BOTTLE_WEIGHT.full)) {
-    console.log('FULL BOTTLE ON SCALE', {waterLevel: 100})
+  if (result) {
+    io.emit(result.event, result.payload)
+    console.log({ result })
   }
 })
 
@@ -97,7 +39,3 @@ port.on('readable', function () {
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
-
-function approxEqual(v1, v2) {
-  return Math.abs(v1 - v2) < TOLERANCE_GRAMS
-}
